223127125ae86f319dde2b562cccfed82a8a8e46
radiospiel <eno@open-lab.org>
2013-12-11 20:11:44 +0100

    Review info controller.

--- api/app/controllers/info.rb
+++ api/app/controllers/info.rb
@@ -1,5 +1,17 @@
 class Controllers::Info < Controllers::Base
   get "/" do
-    "info"
+    headers["Content-Type"] = "text/plain"
+    "pipe2me API #{VERSION}"
+  end
+
+  get "/version" do
+    render(
+      api: "pipe2me #{VERSION}",
+      version: VERSION,
+      domain: DOMAIN,
+      ports: PORTS,
+      ports_per_subdomain: PORTS_PER_SUBDOMAIN
+    )
   end
 end
+
