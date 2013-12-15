48245148f30c0b327cedda6e667cd0a006a6b7b4
radiospiel <eno@open-lab.org>
2013-12-15 17:22:38 +0100

    Clean up logging, source

--- api/lib/sys.rb
+++ api/lib/sys.rb
@@ -2,20 +2,20 @@ require "shellwords"
 
 module Sys
   extend self
-  
+
   class ExitError < RuntimeError; end
 
   def self.log(*args)
     # STDERR.puts args.map(&:shellescape) * " "
   end
-  
+
   def sys!(*args, &block)
     options = args.last.is_a?(Hash) ? args.pop : {}
     stdin = options[:stdin]
 
     r = nil
     Sys.log *args
-    
+
     IO.popen([*args.map(&:to_s)], 'w+') do |io|
       io.set_encoding("BINARY")
 
@@ -24,11 +24,11 @@ module Sys
 
       r = io.read
     end
-  
+
     if $?.exitstatus != 0
       raise ExitError, r
     end
-  
+
     yield r if block
     r
   end
