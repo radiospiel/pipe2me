20f62f6e375674f4939aa3f1a6ecea98edf4d247
radiospiel <eno@open-lab.org>
2014-01-03 17:44:35 +0100

    Improve test infrastructre

--- test/runner.rb
+++ test/runner.rb
@@ -1,6 +1,24 @@
 #!/usr/bin/env ruby
-HERE = File.dirname(__FILE__)
+HERE = File.expand_path File.dirname(__FILE__)
+
 require "test/unit"
+ENV["PATH"] = "#{HERE}/../client/ruby/bin:#{ENV["PATH"]}"
+
+module Pipe2me
+  class TestCase < Test::Unit::TestCase
+    def pipe2me(cmd, options = {})
+      cmd = "pipe2me --config pipe2me-config #{cmd}"
+
+      options.each do |key, value|
+        cmd += " --#{key} #{value}"
+      end
+
+      # puts cmd
+      `#{cmd}`
+    end
+  end
+end
+
 Dir.glob("#{HERE}/**/*_test.rb").sort.each do |file|
   puts file
   load file
