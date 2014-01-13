0cc18ca3beb9fb2d207b19bea107f1357e2381cb
radiospiel <eno@open-lab.org>
2014-01-13 14:49:46 +0100

    Change sshd port to 8444

--- config/initializers/sshd.rb
+++ config/initializers/sshd.rb
@@ -1,8 +1,4 @@
 require "sshd"
 
 SSHD.root = File.join( "#{VAR}/sshd")
-if RACK_ENV == "test"
-  SSHD.listen_address = "#{TUNNEL_DOMAIN}:4455"
-else
-  SSHD.listen_address = "#{TUNNEL_DOMAIN}:4444"
-end
+SSHD.listen_address = "#{TUNNEL_DOMAIN}:8444"
