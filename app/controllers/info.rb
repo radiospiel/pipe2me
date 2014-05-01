54d6e68a9298d655febed7bb4e9b42628911a7bc
eno <eno@open-lab.org>
2014-05-01 19:45:57 +0200

    Improve info page
    The info page now contains more details about pipe2me. It can be 
    sourced into a shell, and is rendered from an IRB template.

--- app/controllers/info.rb
+++ app/controllers/info.rb
@@ -1,21 +1,53 @@
+require "shell_format"
+
 class Controllers::Info < Controllers::Base
+  helpers ShellFormat
+
+  set :sourcefile, __FILE__
+
+  helpers do
+    def url_base
+      if HTTPS_PORT
+        "https://#{request.host}:#{HTTPS_PORT}"
+      else
+        "#{request.scheme}://#{request.host_with_port}"
+      end
+    end
+
+    def openssl_fingerprint(file, mode = :md5)
+      expect! mode => [ :md5, :sha1 ]
+      `openssl x509 -noout -fingerprint -#{mode} -in #{file}`.chomp
+    end
+
+    def pipe2me_config
+      {
+        domain:       TUNNEL_DOMAIN,
+        environment:  RACK_ENV,
+        ports:        TUNNEL_PORTS,
+        version:      VERSION,
+        url:          url_base
+      }
+    end
+  end
+
   get "/" do
     headers["Content-Type"] = "text/plain"
-    "pipe2me API #{VERSION}"
+
+    erb :info
   end
 
   get "/cacert" do
-    headers["Content-Type"] = "text/plain"
-    File.read "#{VAR}/openssl/root/certificate.pem"
+    headers["Content-Type"] = "application/x-x509-ca-cert"
+    File.read("var/miniCA/root.pem")
   end
 
-  get "/version" do
-    render(
-      api: "pipe2me #{VERSION}",
-      version: VERSION,
-      domain: TUNNEL_DOMAIN,
-      ports: TUNNEL_PORTS
-    )
+  get "/config" do
+    json pipe2me_config
   end
+
+  # get "/auth" do
+  #   json(
+  #     auth: "auth"
+  #   )
+  # end
 end
-
